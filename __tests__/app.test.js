c9ce1adb21342a95a2bdc42b84e6ddcfb6601310
Zak Hyslin <rapscalan@gmail.com>
2019-12-20 17:13:47 -0800

    login test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,48 @@
+require('dotenv').config();
 
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('app routes', () => {
+  beforeAll(()=> {
+    connect();
+  });
+  
+  beforeEach(()=> {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(()=> {
+    return mongoose.connection.close();
+  });
+
+  it('can signup a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'test@test.com',
+          __v: 0
+        });
+      });
+  });
+
+  it('can login a user', async() => {
+    const user = await User.create({ email: 'test@test.com', password: 'password' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'test@test.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: user.id,
+          email: 'test@test.com',
+          __v: 0
+        });
+      });
+  });
+});
